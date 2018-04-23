a257eae85bfadf2d49ebef5c43152f6dfcc0aa6c
chang.nersc <changh20@gmail.com>
2018-04-22 22:16:33 -0700

    generating spectra in blocks; fixed some bugs in the nersc stuff

--- run/gamalegacy_spectra.py
+++ run/gamalegacy_spectra.py
@@ -66,30 +66,40 @@ def expSpectra(skycondition='bright', seed=1):
     bgs3 = FM.BGStree() 
     match = bgs3._GamaLegacy(gleg) 
     hasmatch = (match != -999) 
-    print('%i galaxies do not have matches' % (len(match) - np.sum(hasmatch)))
+    print('%i galaxies out of %i do not have matches' % ((len(match) - np.sum(hasmatch)), ngal))
+    
+    n_block = (ngal // 1000) + 1 
     
     # r-band aperture magnitude from Legacy photometry 
     r_mag = UT.flux2mag(gleg['legacy-photo']['apflux_r'][:,1])
     vdisp = np.repeat(100.0, ngal) # velocity dispersions [km/s]
+    
+    for i_block in range(n_block): 
+        print('block %i of %i' % (i_block+1, n_block))
+        in_block = (hasmatch & 
+                (np.arange(ngal) >= i_block * 1000) & 
+                (np.arange(ngal) < (i_block+1) * 1000))
 
-    bgstemp = FM.BGStemplates(wavemin=1500.0, wavemax=2e4)
-    flux, wave, meta = bgstemp.Spectra(r_mag[hasmatch], redshift[hasmatch], vdisp[hasmatch],
-            seed=seed, templateid=match[hasmatch], silent=False) 
-    flux0 = flux.copy()  
-    wave, flux_eml = bgstemp.addEmissionLines(wave, flux, gleg, np.arange(ngal)[hasmatch], 
-            silent=False) 
+        bgstemp = FM.BGStemplates(wavemin=1500.0, wavemax=2e4)
+        flux, wave, meta = bgstemp.Spectra(r_mag[in_block], redshift[in_block], vdisp[in_block],
+                seed=seed, templateid=match[in_block], silent=False) 
+        flux0 = flux.copy()  
+        wave, flux_eml = bgstemp.addEmissionLines(wave, flux, gleg, np.arange(ngal)[in_block], 
+                silent=False) 
 
-    # simulate exposure using 
-    fdesi = FM.fakeDESIspec() 
+        # simulate exposure using 
+        fdesi = FM.fakeDESIspec() 
 
-    f = ''.join([UT.dat_dir(), 
-        'gama_legacy.expSpectra.', skycondition, 'sky.seed', str(seed), '.fits']) 
-    bgs_spectra = fdesi.simExposure(wave, flux_eml, skycondition=skycondition, 
-            filename=f) 
-    # save indices for future reference 
-    f_index = ''.join([UT.dat_dir(), 
-        'gama_legacy.expSpectra.', skycondition, 'sky.seed', str(seed), '.index']) 
-    np.savetxt(f_indx, np.arange(ngal)[hasmatch], fmt='%i')
+        f = ''.join([UT.dat_dir(), 
+            'gama_legacy.expSpectra.', skycondition, 'sky.seed', str(seed), 
+            '.', str(i_block+1), 'of', str(n_block), 'blocks.fits']) 
+        bgs_spectra = fdesi.simExposure(wave, flux_eml, skycondition=skycondition, 
+                filename=f) 
+        # save indices for future reference 
+        f_indx = ''.join([UT.dat_dir(), 
+            'gama_legacy.expSpectra.', skycondition, 'sky.seed', str(seed), 
+            '.', str(i_block+1), 'of', str(n_block), 'blocks.index']) 
+        np.savetxt(f_indx, np.arange(ngal)[in_block], fmt='%i')
     return None 
 
 
