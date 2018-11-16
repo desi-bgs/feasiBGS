1bc3a548d3e66d1c19392e702b111b4168d51aef
changhoonhahn <changhoonhahn@lbl.gov>
2018-11-16 07:59:43 -0800

    SNR of simulated spectra for mock exposures

--- run/local_mockexp_simspec_redrock.sh
+++ run/local_mockexp_simspec_redrock.sh
@@ -2,11 +2,13 @@
 
 dir_spec="/Users/ChangHoon/data/feasiBGS/spectra/gamadr3_legacydr7/"
 
-f_str="g15.sim_spectra.mockexp_block.480.iexp0.KSsky"
-f_spec=$dir_spec$f_str".fits"
-f_redr=$dir_spec$f_str".rr.fits"
-f_zout=$dir_spec$f_str".rr.h5"
-
 export OMP_NUM_THREADS=1 
 
-rrdesi --mp 4 --zbest $f_redr --output $f_zout $f_spec
+for skymodel in "KS" "newKS"; do 
+    f_str="g15.sim_spectra.mockexp_block.480.iexp0."$skymodel"sky"
+    f_spec=$dir_spec$f_str".fits"
+    f_redr=$dir_spec$f_str".rr.fits"
+    f_zout=$dir_spec$f_str".rr.h5"
+
+    rrdesi --mp 4 --zbest $f_redr --output $f_zout $f_spec
+done
