4256692af1ed4dce67fb317e33e3af0540749fd8
changhoonhahn <changhoonhahn@lbl.gov>
2018-11-20 00:08:33 -0800

    testing the redrock outputs of the mock exposure simulate spectra

--- run/local_mockexp_simspec_redrock.sh
+++ run/local_mockexp_simspec_redrock.sh
@@ -4,11 +4,15 @@ dir_spec="/Users/ChangHoon/data/feasiBGS/spectra/gamadr3_legacydr7/"
 
 export OMP_NUM_THREADS=1 
 
-for skymodel in "KS" "newKS"; do 
-    f_str="g15.sim_spectra.mockexp_block.480.iexp0."$skymodel"sky"
-    f_spec=$dir_spec$f_str".fits"
-    f_redr=$dir_spec$f_str".rr.fits"
-    f_zout=$dir_spec$f_str".rr.h5"
+for iexp in 100 200; do 
+    for skymodel in "KS" "newKS"; do 
+        echo "mock exposure #"$iexp
 
-    rrdesi --mp 4 --zbest $f_redr --output $f_zout $f_spec
-done
+        f_str="g15.sim_spectra.mockexp_block.480.iexp"$iexp"."$skymodel"sky"
+        f_spec=$dir_spec$f_str".fits"
+        f_redr=$dir_spec$f_str".rr.fits"
+        f_zout=$dir_spec$f_str".rr.h5"
+
+        rrdesi --mp 4 --zbest $f_redr --output $f_zout $f_spec
+    done
+done 
