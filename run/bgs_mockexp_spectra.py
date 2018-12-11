c50e00adfdbe899ec618cf169ad571aa28c52998
changhoonhahn <changhoonhahn@lbl.gov>
2018-12-10 23:03:57 -0800

    low Halpha bgs gleg simulated spectra

--- run/bgs_mockexp_spectra.py
+++ run/bgs_mockexp_spectra.py
@@ -293,6 +293,97 @@ def mockexp_gleg_simSpec_lowHA(iexp):
     return None 
 
 
+def mockexp_gleg_simSpec_pick(iexp): 
+    ''' simulate DESI BGS spectra with exposure time from survey sim mock exposure
+    '''
+    # mock exposures
+    exps = mockexp() 
+    
+    # GAMA-Legacy catalog
+    cata = Cat.GamaLegacy()
+    gleg = cata.Read('g15', dr_gama=3, dr_legacy=7)
+
+    redshift = gleg['gama-spec']['z']
+    absmag_ugriz = cata.AbsMag(gleg, kcorr=0.1, H0=70, Om0=0.3, galext=False) # ABSMAG k-correct to z=0.1
+    r_mag_apflux = UT.flux2mag(gleg['legacy-photo']['apflux_r'][:,1])
+    r_mag_gama = gleg['gama-photo']['modelmag_r'] # r-band magnitude from GAMA (SDSS) photometry
+    
+    ha_gama = gleg['gama-spec']['ha_flux'] # halpha line flux 
+
+    ngal = len(redshift) # number of galaxies
+    vdisp = np.repeat(100.0, ngal) # velocity dispersions [km/s]
+
+    # match GAMA galaxies to templates 
+    bgs3 = FM.BGStree()
+    match = bgs3._GamaLegacy(gleg)
+    hasmatch = (match != -999)
+    
+    # only the block 
+    nblock = int(np.ceil(float(ngal) / 1000.))
+    in_block = np.random.choice(np.arange(ngal)[hasmatch & (ha_gama <= 0.) & (redshift > 0.19) & (redshift < 0.21) & (r_mag_apflux < 20.65) & (r_mag_apflux > 20.45)], 1, replace=False) 
+
+    # get source spectra  
+    fblock = ''.join([UT.dat_dir(), 'spectra/gamadr3_legacydr7/',
+        'g15.source_spectra.mockexp_block.r_ap20.55.p'])
+    if os.path.isfile(fblock): 
+        flux_eml, wave, magnorm_flag = pickle.load(open(fblock, 'rb'))
+    else: 
+        s_bgs = FM.BGSsourceSpectra(wavemin=1500.0, wavemax=2e4)
+        emline_flux = s_bgs.EmissionLineFlux(gleg, index=np.arange(ngal)[in_block], dr_gama=3, silent=True)
+        flux_eml, wave, _, magnorm_flag = s_bgs.Spectra(r_mag_apflux[in_block], redshift[in_block],
+                                                        vdisp[in_block], seed=1, templateid=match[in_block],
+                                                        emflux=emline_flux, mag_em=r_mag_gama[in_block], 
+                                                        silent=False)
+        pickle.dump([flux_eml, wave, magnorm_flag], open(fblock, 'wb'))
+
+    # write GLeg catalog block for future reference
+    cata_block = cata.select(index=in_block)
+    cata_block['forwardmodel'] = {}
+    cata_block['forwardmodel']['magnorm_flag'] = magnorm_flag
+
+    fblock = ''.join([UT.dat_dir(), 'spectra/gamadr3_legacydr7/', 'g15.mockexp_block.r_ap20.55.hdf5'])
+    if not os.path.isfile(fblock):
+        cata.write(cata_block, fblock)
+    
+    # read in sky surface brightness
+    w_sky, skybright_old = mockexp_skyBright('old')
+    _, skybright_new = mockexp_skyBright('new')
+
+    skybright_old = skybright_old[iexp,:] 
+    skybright_new = skybright_new[iexp,:]
+    u_surface_brightness = 1e-17 * u.erg / u.angstrom / u.arcsec**2 / u.cm**2 / u.second
+
+    # simulate the exposures 
+    fdesi = FM.fakeDESIspec()
+
+    # with old sky model
+    f_simspec_old = ''.join([UT.dat_dir(), 'spectra/gamadr3_legacydr7/',
+        'g15.sim_spectra.mockexp_block.r_ap20.55.texp600.iexp', str(iexp), '.KSsky.fits'])
+    if not os.path.isfile(f_simspec_old): 
+        #bgs_spectra = read_spectra(f_simspec_old)
+        bgs_spectra = fdesi.simExposure(wave, flux_eml, 
+                exptime=600., #exps['EXPTIME'][iexp], 
+                airmass=exps['AIRMASS'][iexp],
+                skycondition={'name': 'input', 
+                    'sky': np.clip(skybright_old, 0, None) * u_surface_brightness, 
+                    'wave': w_sky}, 
+                filename=f_simspec_old)
+
+    # with new sky model 
+    f_simspec_new = ''.join([UT.dat_dir(), 'spectra/gamadr3_legacydr7/',
+        'g15.sim_spectra.mockexp_block.r_ap20.55.texp600.iexp', str(iexp), '.newKSsky.fits'])
+    if not os.path.isfile(f_simspec_new):
+        # bgs_spectra = read_spectra(f_simspec_new)
+        bgs_spectra = fdesi.simExposure(wave, flux_eml, 
+                exptime=600., #exps['EXPTIME'][iexp], 
+                airmass=exps['AIRMASS'][iexp],
+                skycondition={'name': 'input',
+                    'sky': np.clip(skybright_new, 0, None) * u_surface_brightness,
+                    'wave': w_sky},
+                filename=f_simspec_new)
+    return None 
+
+
 def mockexp(): 
     # read exposures from file
     fexp = h5py.File(''.join([UT.dat_dir(), 'bgs_survey_exposures.withsun.hdf5']), 'r')
@@ -317,6 +408,8 @@ def mockexp_skyBright(skymodel):
 
 
 if __name__=="__main__": 
+    mockexp_gleg_simSpec_pick(562)
+    '''
     iblock = sys.argv[1]
     iexp = int(sys.argv[2])
     if iblock == 'lowHA': 
@@ -333,3 +426,4 @@ if __name__=="__main__":
             texp = float(texp) 
             mockexp_gleg_simSpec_texp(iblock, iexp, texp=texp)
         print('--- took %f mins ---' % ((time.time() - t0)/60.))
+    '''
