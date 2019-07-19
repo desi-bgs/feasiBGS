fafa8d9386566d5c973f5549d88149a169f352e5
changhoonhahn <changhoonhahn@lbl.gov>
2019-07-19 11:22:00 -0700

    function to validate forked desisurvey.etc implementation

--- run/bgs_zsuccess/exposures.py
+++ run/bgs_zsuccess/exposures.py
@@ -8,8 +8,13 @@ import os
 import h5py
 import pickle 
 import numpy as np 
+from scipy.interpolate import interp1d
+# --- astropy --- 
+import astropy.units as u
+from astropy.io import fits
 # -- feasibgs -- 
 from feasibgs import util as UT 
+from feasibgs import skymodel as Sky
 # -- plotting -- 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -26,10 +31,11 @@ mpl.rcParams['ytick.major.width'] = 1.5
 mpl.rcParams['legend.frameon'] = False
 
 
-def pickExposures(nsub, method='random', validate=False, silent=True): 
-    ''' Pick nsub subset of exposures from `surveysim` exposure list
-    from Jeremy: `bgs_survey_exposures.withsun.hdf5', which  
-    supplemented the observing conditions with sun observing conditions.
+specsim_sky = Sky.specsim_initialize('desi')
+
+
+def pickExposures(nsub, method='random', validate=False, expfile=None, silent=True): 
+    ''' Pick nsub subset of exposures from `surveysim` BGS exposure list
     Outputs a file that contains the exposure indices, observing conditions, 
     and old and new sky brightness of the chosen subset. 
 
@@ -46,11 +52,14 @@ def pickExposures(nsub, method='random', validate=False, silent=True):
         if False, code will print statements to indicate progress 
     '''
     # read surveysim BGS exposures 
-    fexps = h5py.File(''.join([UT.dat_dir(), 'bgs_survey_exposures.withsun.hdf5']), 'r')
-    bgs_exps = {}
-    for k in fexps.keys():
-        bgs_exps[k] = fexps[k].value
-    n_exps = len(bgs_exps['RA']) # number of exposures
+    bgs_exps = extractBGSsurveysim(expfile)
+    n_exps = len(bgs_exps['ra']) # number of exposures
+    airmass = bgs_exps['airmass'] 
+    moonill = bgs_exps['moon_ill'] 
+    moonalt = bgs_exps['moon_alt'] 
+    moonsep = bgs_exps['moon_sep'] 
+    sun_alt = bgs_exps['sun_alt']
+    sun_sep = bgs_exps['sun_sep']
         
     # pick a small subset
     if method == 'random': # randomly pick exposures
@@ -59,11 +68,11 @@ def pickExposures(nsub, method='random', validate=False, silent=True):
     elif method == 'spacefill': # pick exposures that span the observing conditions
         if not silent: print('picking exposures to span observing conditions')
         obs = np.zeros((n_exps, 4))
-        #obs[:,0] = bgs_exps['AIRMASS']
-        obs[:,0] = bgs_exps['MOONFRAC']
-        obs[:,1] = bgs_exps['MOONALT']
-        obs[:,2] = bgs_exps['MOONSEP']
-        obs[:,3] = bgs_exps['SUNALT']
+        #obs[:,0] = airmass 
+        obs[:,0] = moonill
+        obs[:,1] = moonalt 
+        obs[:,2] = moonsep 
+        obs[:,3] = sun_alt 
         histmd, edges = np.histogramdd(obs[obs[:,1] > 0.,:], 2)
         _hasexp = histmd > 0.
         has_exp = np.where(_hasexp)
@@ -79,75 +88,74 @@ def pickExposures(nsub, method='random', validate=False, silent=True):
         nsub = len(iexp_sub) 
         if not silent: print('%i exposures in the subset' % nsub)
     
-    # read in pre-computed old and new sky brightness (this takes a bit) 
-    if not silent: print('reading in sky brightness') 
-    fold = ''.join([UT.dat_dir(), 'KSsky_brightness.bgs_survey_exposures.withsun.p'])
-    wave_old, sky_old = pickle.load(open(fold, 'rb'))
-    fnew = ''.join([UT.dat_dir(), 'newKSsky_twi_brightness.bgs_survey_exposures.withsun.p'])
-    wave_new, sky_new = pickle.load(open(fnew, 'rb'))
-    
+    # computed sky brightness (this takes a bit) 
+    if not silent: print('computing sky brightness') 
+    Iskys = [] 
+    for i in iexp_sub: 
+        wave, _Isky = sky_KSrescaled_twi(airmass[i], moonill[i], moonalt[i], moonsep[i], sun_alt[i], sun_sep[i])
+        Iskys.append(_Isky)
+    print(wave.min(), wave.max())
+    Iskys = np.array(Iskys)
     # write exposure subsets out to file 
-    fpick = h5py.File(''.join([UT.dat_dir(), 'bgs_zsuccess/', 
-        'bgs_survey_exposures.subset.', str(nsub), method, '.hdf5']), 'w')
-    fpick.create_dataset('iexp', data=iexp_sub) 
-    for k in ['AIRMASS', 'MOONFRAC', 'MOONALT', 'MOONSEP', 'SUNALT', 'SUNSEP', 'EXPTIME']: # write observing conditions  
+    fpick = h5py.File(os.path.join(UT.dat_dir(), 'bgs_zsuccess/', 
+        'surveysim_bgs_exposures.subset.%i%s.hdf5' % (nsub, method)), 'w')
+    # write observing conditions 
+    for k in ['airmass', 'moon_ill', 'moon_alt', 'moon_sep', 'sun_alt', 'sun_sep', 'texp']:  
         fpick.create_dataset(k.lower(), data=bgs_exps[k][iexp_sub]) 
     # save sky brightnesses
-    fpick.create_dataset('wave_old', data=wave_old) 
-    fpick.create_dataset('wave_new', data=wave_new) 
-    fpick.create_dataset('sky_old', data=sky_old[iexp_sub,:]) 
-    fpick.create_dataset('sky_new', data=sky_new[iexp_sub,:]) 
+    fpick.create_dataset('wave', data=wave) 
+    fpick.create_dataset('sky', data=Iskys) 
     fpick.close() 
 
     if validate: 
         fig = plt.figure(figsize=(21,5))
         sub = fig.add_subplot(141)
-        sub.scatter(bgs_exps['MOONALT'], bgs_exps['MOONFRAC'], c='k', s=1)
-        scat = sub.scatter(bgs_exps['MOONALT'][iexp_sub], bgs_exps['MOONFRAC'][iexp_sub], c='C1', s=10)
+        sub.scatter(bgs_exps['moon_alt'], bgs_exps['moon_ill'], c='k', s=1)
+        scat = sub.scatter(bgs_exps['moon_alt'][iexp_sub], bgs_exps['moon_ill'][iexp_sub], c='C1', s=10)
         sub.set_xlabel('Moon Altitude', fontsize=20)
         sub.set_xlim([-90., 90.])
         sub.set_ylabel('Moon Illumination', fontsize=20)
         sub.set_ylim([0.5, 1.])
 
         sub = fig.add_subplot(142)
-        sub.scatter(bgs_exps['MOONSEP'], bgs_exps['MOONFRAC'], c='k', s=1)
-        scat = sub.scatter(bgs_exps['MOONSEP'][iexp_sub], bgs_exps['MOONFRAC'][iexp_sub], c='C1', s=10) 
+        sub.scatter(bgs_exps['moon_sep'], bgs_exps['moon_ill'], c='k', s=1)
+        scat = sub.scatter(bgs_exps['moon_sep'][iexp_sub], bgs_exps['moon_ill'][iexp_sub], c='C1', s=10) 
         sub.set_xlabel('Moon Separation', fontsize=20)
         sub.set_xlim([40., 180.])
         sub.set_ylim([0.5, 1.])
 
         sub = fig.add_subplot(143)
-        sub.scatter(bgs_exps['AIRMASS'], bgs_exps['MOONFRAC'], c='k', s=1)
-        scat = sub.scatter(bgs_exps['AIRMASS'][iexp_sub], bgs_exps['MOONFRAC'][iexp_sub], c='C1', s=10)  
+        sub.scatter(bgs_exps['airmass'], bgs_exps['moon_ill'], c='k', s=1)
+        scat = sub.scatter(bgs_exps['airmass'][iexp_sub], bgs_exps['moon_ill'][iexp_sub], c='C1', s=10)  
         sub.set_xlabel('Airmass', fontsize=20)
         sub.set_xlim([1., 2.])
         sub.set_ylim([0.5, 1.])
 
         sub = fig.add_subplot(144)
-        sub.scatter(bgs_exps['SUNSEP'], bgs_exps['SUNALT'], c='k', s=1)
-        scat = sub.scatter(bgs_exps['SUNSEP'][iexp_sub], bgs_exps['SUNALT'][iexp_sub], c='C1', s=10)
+        sub.scatter(bgs_exps['sun_sep'], bgs_exps['sun_alt'], c='k', s=1)
+        scat = sub.scatter(bgs_exps['sun_sep'][iexp_sub], bgs_exps['sun_alt'][iexp_sub], c='C1', s=10)
         sub.set_xlabel('Sun Separation', fontsize=20)
         sub.set_xlim([40., 180.])
         sub.set_ylabel('Sun Altitude', fontsize=20)
         sub.set_ylim([-90., 0.])
-        fig.savefig(''.join([UT.dat_dir(), 'bgs_zsuccess/', 'bgs_survey_exposures.subset.', str(nsub), method, '.png']), 
-                bbox_inches='tight')
+        fig.savefig(''.join([UT.dat_dir(), 'bgs_zsuccess/', 
+            'surveysim_bgs_exposures.subset.', str(nsub), method, '.png']), 
+            bbox_inches='tight')
 
         # plot some of the sky brightnesses
-        fig = plt.figure(figsize=(15,20))
+        fig = plt.figure(figsize=(15,5))
         bkgd = fig.add_subplot(111, frameon=False) 
-        for ii, isky in enumerate(np.random.choice(iexp_sub, 4, replace=False)):
-            sub = fig.add_subplot(4,1,ii+1)
-            sub.plot(wave_old, sky_new[isky,:], c='C1', label='new sky')
-            sub.plot(wave_old, sky_old[isky,:], c='k', label='old sky')
-            sub.set_xlim([3500., 9500.]) 
-            sub.set_ylim([0., 20]) 
-            if ii == 0: sub.legend(loc='upper left', fontsize=20) 
+        for isky in range(Iskys.shape[0]):
+            sub = fig.add_subplot(111)
+            sub.plot(wave, Iskys[isky,:])
+        sub.set_xlim([3500., 9500.]) 
+        sub.set_ylim([0., 20]) 
         bkgd.tick_params(labelcolor='none', top=False, bottom=False, left=False, right=False)
         bkgd.set_xlabel('wavelength [Angstrom]', fontsize=25) 
         bkgd.set_ylabel('sky brightness [$erg/s/cm^2/A/\mathrm{arcsec}^2$]', fontsize=25) 
-        fig.savefig(''.join([UT.dat_dir(), 'bgs_zsuccess/', 'bgs_survey_exposures.subset.', str(nsub), method, '.skybright.png']), 
-                bbox_inches='tight')
+        fig.savefig(''.join([UT.dat_dir(), 'bgs_zsuccess/', 
+            'surveysim_bgs_exposures.subset.', str(nsub), method, '.skybright.png']), 
+            bbox_inches='tight')
     return None 
 
 
@@ -269,9 +277,247 @@ def tableExposures(nsub, method='spacefill'):
     return None 
 
 
+def extractBGSsurveysim(fname): 
+    """ extra data on bgs exposures from surveysim output 
+
+    no cosmics split adds 20% to margin
+    total BGS time: 2839 hours
+    total BGS minus twilight: 2372
+    assuming 7.5 deg^2 per field
+    assumiong 68% open-dome fraction
+    """
+    ssout = fits.open(fname)[1].data # survey sim output 
+    tiles = fits.open(os.path.join(UT.dat_dir(), 'bright_exposure', 'desi-tiles.fits'))[1].data # desi-tiles
+    
+    isbgs = (tiles['PROGRAM'] == 'BRIGHT') # only bgs 
+    
+    uniq_tiles, iuniq = np.unique(ssout['TILEID'], return_index=True)  
+    _, ssbgs, bgsss = np.intersect1d(ssout['TILEID'][iuniq], tiles['TILEID'][isbgs], return_indices=True)  
+    
+    RAs, DECs = [], [] 
+    airmasses, moon_ills, moon_alts, moon_seps, sun_alts, sun_seps, texps = [], [], [], [], [], [], []
+    nexps = 0 
+    for i in range(len(ssbgs)): 
+        isexps  = (ssout['TILEID'] == ssout['TILEID'][iuniq][ssbgs[i]]) 
+        nexp    = np.sum(isexps)
+        ra      = tiles['RA'][isbgs][bgsss[i]]
+        dec     = tiles['DEC'][isbgs][bgsss[i]]
+        mjd     = ssout['MJD'][isexps]
+        # get sky parameters for given ra, dec, and mjd
+        moon_ill, moon_alt, moon_sep, sun_alt, sun_sep = get_thetaSky(np.repeat(ra, nexp), np.repeat(dec, nexp), mjd)
+
+        texps.append(ssout['EXPTIME'][isexps]) 
+        RAs.append(np.repeat(ra, nexp)) 
+        DECs.append(np.repeat(dec, nexp)) 
+        airmasses.append(ssout['AIRMASS'][isexps]) 
+        moon_ills.append(moon_ill)
+        moon_alts.append(moon_alt) 
+        moon_seps.append(moon_sep)
+        sun_alts.append(sun_alt) 
+        sun_seps.append(sun_sep) 
+
+        nexps += nexp 
+
+    exps = {
+        'texp':     np.concatenate(texps), 
+        'ra':       np.concatenate(RAs),
+        'dec':      np.concatenate(DECs),
+        'airmass':  np.concatenate(airmasses),
+        'moon_ill': np.concatenate(moon_ills), 
+        'moon_alt': np.concatenate(moon_alts), 
+        'moon_sep': np.concatenate(moon_seps), 
+        'sun_alt':  np.concatenate(sun_alts), 
+        'sun_sep':  np.concatenate(sun_seps)
+    }
+    return exps 
+
+
+def sky_KSrescaled_twi(airmass, moonill, moonalt, moonsep, sun_alt, sun_sep):
+    ''' calculate sky brightness using rescaled KS coefficients plus a twilight
+    factor from Parker. 
+
+    :return specsim_wave, Isky: 
+        returns wavelength [Angstrom] and sky surface brightness [$10^{-17} erg/cm^{2}/s/\AA/arcsec^2$]
+    '''
+    #specsim_sky = Sky.specsim_initialize('desi')
+    specsim_wave = specsim_sky._wavelength # Ang
+    specsim_sky.airmass = airmass
+    specsim_sky.moon.moon_phase = np.arccos(2.*moonill - 1)/np.pi
+    specsim_sky.moon.moon_zenith = (90. - moonalt) * u.deg
+    specsim_sky.moon.separation_angle = moonsep * u.deg
+    
+    # updated KS coefficients 
+    specsim_sky.moon.KS_CR = 458173.535128
+    specsim_sky.moon.KS_CM0 = 5.540103
+    specsim_sky.moon.KS_CM1 = 178.141045
+        
+
+    _sky = specsim_sky._surface_brightness_dict['dark'].copy()
+    _sky *= specsim_sky.extinction
+
+    I_ks_rescale = specsim_sky.surface_brightness
+    Isky = I_ks_rescale.value
+    if sun_alt > -20.: # adding in twilight
+        w_twi, I_twi = cI_twi(sun_alt, sun_sep, airmass)
+        I_twi /= np.pi
+        I_twi_interp = interp1d(10. * w_twi, I_twi, fill_value='extrapolate')
+        Isky += np.clip(I_twi_interp(specsim_wave), 0, None) 
+    return specsim_wave, Isky
+
+
+def cI_twi(alpha, delta, airmass):
+    ''' twilight contribution
+
+    :param alpha: 
+
+    :param delta: 
+
+    :param airmass: 
+
+    :return twi: 
+
+    '''
+    ftwi = os.path.join(UT.dat_dir(), 'sky', 'twilight_coeffs.p')
+    twi_coeffs = pickle.load(open(ftwi, 'rb'))
+    twi = (
+        twi_coeffs['t0'] * np.abs(alpha) +      # CT2
+        twi_coeffs['t1'] * np.abs(alpha)**2 +   # CT1
+        twi_coeffs['t2'] * np.abs(delta)**2 +   # CT3
+        twi_coeffs['t3'] * np.abs(delta)        # CT4
+    ) * np.exp(-twi_coeffs['t4'] * airmass) + twi_coeffs['c0']
+    return twi_coeffs['wave'], np.array(twi)
+
+
+def _twilight_coeffs(): 
+    ''' save twilight coefficients from Parker
+    '''
+    import pandas as pd
+    f = os.path.join(UT.code_dir(), 'dat', 'sky', 'MoonResults.csv')
+
+    coeffs = pd.DataFrame.from_csv(f)
+    coeffs.columns = [
+        'wl', 'model', 'data_var', 'unexplained_var',' X2', 'rX2',
+        'c0', 'c_am', 'tau', 'tau2', 'c_zodi', 'c_isl', 'sol', 'I',
+        't0', 't1', 't2', 't3', 't4', 'm0', 'm1', 'm2', 'm3', 'm4', 'm5', 'm6',
+        'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec',
+        'c2', 'c3', 'c4', 'c5', 'c6']
+    # keep moon models
+    twi_coeffs = coeffs[coeffs['model'] == 'twilight']
+    coeffs = coeffs[coeffs['model'] == 'moon']
+    # order based on wavelengths for convenience
+    wave_sort = np.argsort(np.array(coeffs['wl']))
+
+    twi = {} 
+    twi['wave'] = np.array(coeffs['wl'])[wave_sort] 
+    for k in ['t0', 't1', 't2', 't3', 't4', 'c0']:
+        twi[k] = np.array(twi_coeffs[k])[wave_sort]
+    
+    # save to file 
+    ftwi = os.path.join(UT.dat_dir(), 'sky', 'twilight_coeffs.p')
+    pickle.dump(twi, open(ftwi, 'wb'))
+    return None 
+
+
+def get_thetaSky(ra, dec, mjd): 
+    ''' given RA, Dec, and mjd time return sky parameters at kitt peak 
+    '''
+    import ephem 
+    from astropy.time import Time
+    import desisurvey.config
+    import desisurvey.utils as dutils
+    config = desisurvey.config.Configuration()
+
+    mayall = ephem.Observer()
+    mayall.lat = config.location.latitude().to(u.rad).value
+    mayall.lon = config.location.longitude().to(u.rad).value
+    mayall.elevation = config.location.elevation().to(u.m).value
+    # Configure atmospheric refraction model for rise/set calculations.
+    mayall.pressure = 1e3 * config.location.pressure().to(u.bar).value
+    mayall.temp = config.location.temperature().to(u.C).value
+
+    # observed time (MJD) 
+    mjd_time = Time(mjd, format='mjd')
+
+    moon_alt    = np.zeros(len(mjd))
+    moon_ra     = np.zeros(len(mjd))
+    moon_dec    = np.zeros(len(mjd)) 
+    moon_ill    = np.zeros(len(mjd))
+    sun_alt     = np.zeros(len(mjd))
+    sun_ra      = np.zeros(len(mjd))
+    sun_dec     = np.zeros(len(mjd)) 
+    for i in range(len(mjd)):
+        mayall.date = mjd_time.datetime[i] 
+        _moon = ephem.Moon()
+        _moon.compute(mayall) 
+        _sun = ephem.Sun()
+        _sun.compute(mayall) 
+        
+        moon_alt[i] = 180./np.pi*_moon.alt
+        moon_ra[i]  = 180./np.pi*_moon.ra
+        moon_dec[i] = 180./np.pi*_moon.dec
+        moon_ill[i] = _moon.moon_phase
+        sun_alt[i] = 180./np.pi*_sun.alt
+        sun_ra[i]  = 180./np.pi*_sun.ra
+        sun_dec[i] = 180./np.pi*_sun.dec
+
+    moon_sep    = np.diag(dutils.separation_matrix(moon_ra, moon_dec, np.atleast_1d(ra), np.atleast_1d(dec)))
+    sun_sep     = np.diag(dutils.separation_matrix(sun_ra, sun_dec, np.atleast_1d(ra), np.atleast_1d(dec)))
+    return moon_ill, moon_alt, moon_sep, sun_alt, sun_sep
+
+
+def validate_desisurvey_etc(): 
+    ''' running desisurvey.etc and validating with implementaiton of sky model in this script. 
+    This doesn't really belong here -- oh well. 
+    '''
+    import time 
+    #t0 = time.time() 
+    #w, Isky0 = sky_KSrescaled_twi(1.2, 0.7, 60., 100., -30., 100.)
+    #print('sky model takes %f sec' % ((time.time() - t0)))
+    #t0 = time.time() 
+    #_, Isky1 = Sky._Isky(1.2, 0.7, 60., 100.)
+    #print('light sky model takes %f sec' % ((time.time() - t0)))
+    #print(Isky0)
+    #print(Isky1)
+    from desisurvey import etc 
+    #wlim = (w.value > 4000.) & (w.value < 5000.) 
+    #print np.median(Isky0[wlim])/1.519
+    #wlim = (w > 4000.) & (w < 5000.) 
+    #print np.median(Isky1.value[wlim])/1.519
+    #print etc.texp_factor_bright_notwi(1.2, 0.7, 60., 100.) 
+
+    #w, Isky0 = sky_KSrescaled_twi(1.2, 0.7, 60., 100., -15., 100.)
+    #wlim = (w.value > 4000.) & (w.value < 5000.) 
+    print(etc.texp_factor_bright_notwi(1.2, 0.7, 60., 100.))
+    t0 = time.time() 
+    print(etc.texp_factor_bright_notwi(
+        np.array([1.2, 1.2]), 
+        np.array([0.7, 0.7]), 
+        np.array([60., 60.]), 
+        np.array([100., 100.]))
+        ) 
+    print('texp_factor_bright_notwi takes %f sec' % ((time.time() - t0)))
+    #print(np.median(Isky0[wlim])/1.519)
+    print(etc.texp_factor_bright_twi(1.2, 0.7, 60., 100., -15., 100.)) 
+    t0 = time.time() 
+    print(etc.texp_factor_bright_twi(
+        np.array([1.2, 1.2]), 
+        np.array([0.7, 0.7]), 
+        np.array([60., 60.]), 
+        np.array([100., 100.]), 
+        np.array([-15., -15]), 
+        np.array([100., 100.]))) 
+    print('texp_factor_bright_notwi takes %f sec' % ((time.time() - t0)))
+    #t0 = time.time() 
+    #sky_KSrescaled_twi(1.2, 0.7, 60., 100., -15., 100.)
+    #print('sky model w/ twi takes %f sec' % ((time.time() - t0)))
+    return None 
+
 
 if __name__=="__main__": 
     #pickExposures(10, method='random', silent=False, validate=True)
     #pickExposures(10, method='spacefill', silent=False, validate=True)
     #plotExposures(15, method='spacefill') 
-    tableExposures(15, method='spacefill')
+    #tableExposures(15, method='spacefill')
+    #fexp = os.path.join(UT.dat_dir(), 'bright_exposure', 'exposures_surveysim_fork_150sv0p2.fits') 
+    #extractBGSsurveysim(fexp) 
+    #pickExposures(15, method='spacefill', validate=True, expfile=fexp, silent=False)
