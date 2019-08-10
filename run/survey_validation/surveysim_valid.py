0437b163ca7a6812d8ef626ee9b53c64d7734ef5
changhoonhahn <changhoonhahn@lbl.gov>
2019-08-10 10:04:55 -0700

    only tiles that finish with a single exposure are chosen

--- run/survey_validation/surveysim_valid.py
+++ run/survey_validation/surveysim_valid.py
@@ -7,7 +7,8 @@ import h5py
 import numpy as np 
 import scipy as sp 
 import corner as DFM 
-import desisurvey.etc as detc
+# --- desisurvey --- 
+from desisurvey import etc as ETC
 # --- astropy --- 
 import astropy.units as u
 from astropy.io import fits
@@ -64,13 +65,13 @@ def GALeg_bgsSpec(specfile='GALeg.g15.sourceSpec.3000.hdf5', expfile=None, seed=
     # read in no noise spectra
     _fspec = os.path.join(dir_dat, specfile) 
     fspec = h5py.File(_fspec, 'r') 
-    wave = fspec['wave'].value 
-    flux = fspec['flux'].value 
+    wave = fspec['wave'][...]
+    flux = fspec['flux'][...] 
 
     # read in sampled exposures
     _fsample = os.path.join(dir_dat, expfile.replace('.fits', '.sample.seed%i.fits' % seed)) 
     fexps       = h5py.File(_fsample, 'r')
-    texp        = fexps['texp'][...]
+    texp        = fexps['texp_total'][...]
     airmass     = fexps['airmass'][...]
     moon_ill    = fexps['moon_ill'][...]
     moon_alt    = fexps['moon_alt'][...]
@@ -87,8 +88,8 @@ def GALeg_bgsSpec(specfile='GALeg.g15.sourceSpec.3000.hdf5', expfile=None, seed=
 
     print('--- simulate exposures with sky model ---') 
     for iexp in range(n_sample): 
-        print('t_exp=%f' % texp[iexp])
-        print('airmass=%f' % airmass[iexp])
+        print('t_exp=%.f' % texp[iexp])
+        print('airmass=%.2f' % airmass[iexp])
         print('moon ill=%.2f alt=%.f, sep=%.f' % (moon_ill[iexp], moon_alt[iexp], moon_sep[iexp]))
         print('sun alt=%.f, sep=%.f' % (sun_alt[iexp], sun_sep[iexp]))
         print('seeing=%.2f, transp=%.2f' % (seeing[iexp], transp[iexp]))
@@ -230,7 +231,9 @@ def GALeg_sourceSpec(nsub, validate=False):
 
 
 def sample_surveysimExposures(expfile, seed=0): 
-    '''sample BGS exposures of the surveysim output to cover a wide set of parameter combinations
+    '''sample BGS exposures of the surveysim output to cover a wide set of parameter 
+    combinations. We only selet tiles where SNR2FRAC is achieved with a single exposure
+    due to the fact that the observing conditions are updated when a tile is revsited.  
     '''
     np.random.seed(seed)
     fexp = os.path.join(dir_dat, expfile)
@@ -245,7 +248,9 @@ def sample_surveysimExposures(expfile, seed=0):
     moonalt_bins = [0.0, 40., 90.] 
     sun_alt_bins = [0.0, -20., -90.]
     
-    i_samples = [] 
+    texp_total, airmass = [], [] 
+    moon_ill, moon_alt, moon_sep, sun_alt, sun_sep = [], [], [], [], [] 
+    seeing, transp = [], []
     for i0 in range(len(moonill_bins)-1): 
         for i1 in range(len(moonalt_bins)-1): 
             for i2 in range(len(sun_alt_bins)-1): 
@@ -257,31 +262,84 @@ def sample_surveysimExposures(expfile, seed=0):
                         (exps['sun_alt'] < sun_alt_bins[i2]) & 
                         (exps['sun_alt'] > sun_alt_bins[i2+1]) 
                         )
-                _i_samples = np.random.choice(np.arange(n_exps)[cut & inbin])
-                i_samples.append(_i_samples) 
-                print('moon ill=%.2f, alt=%.f' % (exps['moon_ill'][_i_samples], exps['moon_alt'][_i_samples]))
-                print('sun alt=%.f' % exps['sun_alt'][_i_samples])
-    i_samples = np.array(i_samples) 
+                found_exposures = False
+                while not found_exposures:
+                    # randomly choose an exposure observed at this condition 
+                    _i_samples = np.random.choice(np.arange(n_exps)[cut & inbin])
+                    # find all the exposures of this tile  
+                    same_tile = (exps['tileid'] == exps['tileid'][_i_samples]) 
+                    if (np.sum(same_tile) == 1): 
+                        # SNR2FRAC > 1 was achieved with a single exposure 
+                        _texp_total = np.sum(exps['texp'][same_tile])
+                        _airmass    = exps['airmass'][_i_samples]
+                        _moon_ill   = exps['moon_ill'][_i_samples]
+                        _moon_alt   = exps['moon_alt'][_i_samples]
+                        _moon_sep   = exps['moon_sep'][_i_samples]
+                        _sun_alt    = exps['sun_alt'][_i_samples]
+                        _sun_sep    = exps['sun_sep'][_i_samples]
+                        _seeing     = exps['seeing'][_i_samples]
+                        _transp     = exps['transp'][_i_samples]
+
+                        found_exposures=True 
+
+                print('total t_exp=%.f' % _texp_total)
+                print('airmass = %.1f' % _airmass)
+                fairmass = ETC.airmass_exposure_factor(_airmass) 
+                print('airmass factor = %.1f' % fairmass) 
+
+                print('moon ill=%.2f, alt=%.f' % (_moon_ill, _moon_alt))
+                print('sun alt=%.f' % _sun_alt)
+                fbright = ETC.bright_exposure_factor(_moon_ill, _moon_alt, np.array(_moon_sep),
+                        _sun_alt, _sun_sep, np.array(_airmass))
+                print('bright factor = %.1f' % fbright) 
+
+                print('seeing=%.1f, transp=%.1f' % (_seeing, _transp))
+                _ETC = ETC.ExposureTimeCalculator() 
+                fweather = _ETC.weather_factor(_seeing, _transp) 
+                print('weather factor = %.1f' % fweather) 
+                print('f_total = %.1f' % (fairmass * fbright / fweather))
+                print('t_exp x f_total = %.1f' % (150. * fairmass * fbright / fweather))
+                print('----------------------------') 
+
+                texp_total.append(_texp_total) 
+                airmass.append(_airmass) 
+                moon_ill.append(_moon_ill)
+                moon_alt.append(_moon_alt)
+                moon_sep.append(_moon_sep)
+                sun_alt.append(_sun_alt)
+                sun_sep.append(_sun_sep)
+                seeing.append(_seeing)
+                transp.append(_transp)
+
+    texp_total = np.array(texp_total)
+    airmass    = np.array(airmass)
+    moon_ill   = np.array(moon_ill)
+    moon_alt   = np.array(moon_alt)
+    moon_sep   = np.array(moon_sep)
+    sun_alt    = np.array(sun_alt)
+    sun_sep    = np.array(sun_sep)
+    seeing     = np.array(seeing)
+    transp     = np.array(transp)
 
     # compute sky brightness of the sampled exposures 
     Iskys = [] 
-    for i in i_samples: 
-        wave, _Isky = Sky.sky_KSrescaled_twi(
-                exps['airmass'][i], 
-                exps['moon_ill'][i], 
-                exps['moon_alt'][i], 
-                exps['moon_sep'][i], 
-                exps['sun_alt'][i], 
-                exps['sun_sep'][i])
+    for i in range(len(texp_total)): 
+        wave, _Isky = Sky.sky_KSrescaled_twi(airmass[i], moon_ill[i], moon_alt[i], moon_sep[i], sun_alt[i], sun_sep[i])
         Iskys.append(_Isky)
     Iskys = np.array(Iskys)
     
     # save to file 
     _fsample = fexp.replace('.fits', '.sample.seed%i.fits' % seed)
     fsample = h5py.File(_fsample, 'w') 
-    for k in ['ra', 'dec', 'airmass', 'moon_ill', 'moon_alt', 'moon_sep', 
-            'sun_alt', 'sun_sep', 'texp', 'seeing', 'transp']:  
-        fsample.create_dataset(k.lower(), data=exps[k][i_samples]) 
+    fsample.create_dataset('texp_total', data=texp_total)
+    fsample.create_dataset('airmass', data=airmass)  
+    fsample.create_dataset('moon_ill', data=moon_ill) 
+    fsample.create_dataset('moon_alt', data=moon_alt) 
+    fsample.create_dataset('moon_sep', data=moon_sep)
+    fsample.create_dataset('sun_alt', data=sun_alt) 
+    fsample.create_dataset('sun_sep', data=sun_sep)
+    fsample.create_dataset('seeing', data=seeing)  
+    fsample.create_dataset('transp', data=transp) 
     # save sky brightnesses
     fsample.create_dataset('wave', data=wave) 
     fsample.create_dataset('sky', data=Iskys) 
@@ -290,7 +348,7 @@ def sample_surveysimExposures(expfile, seed=0):
     fig = plt.figure(figsize=(21,5))
     sub = fig.add_subplot(141)
     sub.scatter(exps['moon_alt'], exps['moon_ill'], c='k', s=1)
-    scat = sub.scatter(exps['moon_alt'][i_samples], exps['moon_ill'][i_samples], c='C1', s=10)
+    scat = sub.scatter(moon_alt, moon_ill, c='C1', s=10)
     sub.set_xlabel('Moon Altitude', fontsize=20)
     sub.set_xlim([-90., 90.])
     sub.set_ylabel('Moon Illumination', fontsize=20)
@@ -298,21 +356,21 @@ def sample_surveysimExposures(expfile, seed=0):
 
     sub = fig.add_subplot(142)
     sub.scatter(exps['moon_sep'], exps['moon_ill'], c='k', s=1)
-    scat = sub.scatter(exps['moon_sep'][i_samples], exps['moon_ill'][i_samples], c='C1', s=10) 
+    scat = sub.scatter(moon_sep, moon_ill, c='C1', s=10) 
     sub.set_xlabel('Moon Separation', fontsize=20)
     sub.set_xlim([40., 180.])
     sub.set_ylim([0.5, 1.])
 
     sub = fig.add_subplot(143)
     sub.scatter(exps['airmass'], exps['moon_ill'], c='k', s=1)
-    scat = sub.scatter(exps['airmass'][i_samples], exps['moon_ill'][i_samples], c='C1', s=10)  
+    scat = sub.scatter(airmass, moon_ill, c='C1', s=10)  
     sub.set_xlabel('Airmass', fontsize=20)
     sub.set_xlim([1., 2.])
     sub.set_ylim([0.5, 1.])
 
     sub = fig.add_subplot(144)
     sub.scatter(exps['sun_sep'], exps['sun_alt'], c='k', s=1)
-    scat = sub.scatter(exps['sun_sep'][i_samples], exps['sun_alt'][i_samples], c='C1', s=10)
+    scat = sub.scatter(sun_sep, sun_alt, c='C1', s=10)
     sub.set_xlabel('Sun Separation', fontsize=20)
     sub.set_xlim([40., 180.])
     sub.set_ylabel('Sun Altitude', fontsize=20)
@@ -336,6 +394,64 @@ def sample_surveysimExposures(expfile, seed=0):
     fig.savefig(ffig, bbox_inches='tight')
     return None 
 
+
+def surveysim_BGS_texp(expfile): 
+    ''' a closer examination of the exposure times that are coming otu of surveysim 
+    '''
+    # read in exposures output from surveysim 
+    print('--- %s ---' % expfile) 
+    fexp = os.path.join(dir_dat, expfile)
+    exps = extractBGS(fexp) # get BGS exposures only 
+        
+    u_tileid = np.unique(exps['tileid'])  
+    
+    texp_total = np.zeros(len(u_tileid))
+    exp_factor = np.zeros(len(u_tileid)) 
+    same_day = np.zeros(len(u_tileid)).astype(bool) 
+    single_exp = np.zeros(len(u_tileid)).astype(bool) 
+    for i, tileid in enumerate(u_tileid): 
+        sametile = (exps['tileid'] == tileid) 
+        
+        # total exposure time 
+        texp_total[i] = np.sum(exps['texp'][sametile]) 
+        
+        # all exposures were on the same night 
+        if ((exps['mjd'][sametile].max() - exps['mjd'][sametile].min()) < 1.): 
+            same_day[i] = True
+
+        if np.sum(sametile) == 1: 
+            single_exp[i] = True
+        
+        # observing conditions of first exposure (not reflect of all the exposures but oh well)
+        iexp = np.arange(len(exps['texp']))[sametile][0]
+        fdust = ETC.dust_exposure_factor(exps['ebv'][iexp]) 
+        fairmass = ETC.airmass_exposure_factor(exps['airmass'][iexp]) 
+        fbright = ETC.bright_exposure_factor(
+                exps['moon_ill'][iexp], exps['moon_alt'][iexp], np.array(exps['moon_sep'][iexp]),
+                exps['sun_alt'][iexp], np.array(exps['sun_sep'][iexp]), np.array(exps['airmass'][iexp]))
+        _ETC = ETC.ExposureTimeCalculator() 
+        fweather = _ETC.weather_factor(exps['seeing'][iexp], exps['transp'][iexp])
+        exp_factor[i] = fdust * fairmass * fbright / fweather 
+        if np.sum(sametile) == 1: 
+            print(texp_total[i], 150.*exp_factor[i], exps['snr2frac'][sametile].max())
+
+    # exposure time vs various properties 
+    fig = plt.figure(figsize=(5,5)) 
+    sub = fig.add_subplot(111) 
+    sub.scatter(texp_total, 150. * exp_factor, s=1, c='C0') 
+    sub.scatter(texp_total[same_day], 150. * exp_factor[same_day], s=1, c='C1') 
+    sub.scatter(texp_total[single_exp], 150. * exp_factor[single_exp], s=1, c='C2') 
+    sub.plot([0., 5000.], [0., 5000.], c='k', ls='--', zorder=10) 
+    sub.set_xlabel(r'total $t_{\rm exp}$ (sec)', fontsize=20) 
+    sub.set_xlim(-100., 2500) 
+    sub.set_ylabel(r'(150 sec) $\times f_{\rm exp}$', fontsize=20) 
+    sub.set_ylim(-100., 2500) 
+
+    ffig = os.path.join(dir_dat, 'texp_test.BGS.%s' % expfile.replace('.fits', '.png'))
+    fig.savefig(ffig, bbox_inches='tight') 
+    return None 
+
+
 # --- surveysim output --- 
 def surveysim_BGS(expfile): 
     ''' read in surveysim output exposures and plot the exposure time distributions 
@@ -350,19 +466,29 @@ def surveysim_BGS(expfile):
     exps = extractBGS(fexp) # get BGS exposures only 
         
     twilight = (exps['sun_alt'] >= -20.) 
+    
+    # compile total exposures 
+    texp_tot_master = [] 
+    u_tileid = np.unique(exps_master['tileid'])
+    for tileid in u_tileid: 
+        istile = (exps_master['tileid'] == tileid) 
+        texp_tot_master.append(np.sum(exps_master['texp'][istile]))
 
-    print('master: %.f < texp < %.f' % (exps_master['texp'].min(), exps_master['texp'].max()))
-    print('forked: %.f < texp < %.f' % (exps['texp'].min(), exps['texp'].max()))
+    texp_tot = [] 
+    u_tileid = np.unique(exps['tileid'])
+    for tileid in u_tileid: 
+        istile = (exps['tileid'] == tileid) 
+        texp_tot.append(np.sum(exps['texp'][istile]))
     
     # histogram of total exposures: 
     fig = plt.figure(figsize=(5,5))
     sub = fig.add_subplot(111)
-    sub.hist(exps_master['texp'], bins=50, density=True, range=(0, 2500), color='C0', label='master branch')
-    sub.hist(exps['texp'], bins=50, density=True, range=(0, 2500), alpha=0.75, color='C1', label=r'surveysim fork')
+    sub.hist(texp_tot_master, bins=50, density=True, range=(0, 2500), color='C0', label='master branch')
+    sub.hist(texp_tot, bins=50, density=True, range=(0, 2500), alpha=0.75, color='C1', label=r'surveysim fork')
     sub.legend(loc='upper right', fontsize=20) 
-    sub.set_xlabel(r'$t_{\rm exp}$ (sec) ', fontsize=20) 
+    sub.set_xlabel(r'total $t_{\rm exp}$ (sec) of tiles', fontsize=20) 
     sub.set_xlim(0.,2500) 
-    fig.savefig(os.path.join(dir_dat, 'texp.BGS.%s.png' % expfile.replace('.fits', '')), bbox_inches='tight')
+    fig.savefig(os.path.join(dir_dat, 'texp_total.BGS.%s.png' % expfile.replace('.fits', '')), bbox_inches='tight')
     
     # exposure time vs various properties 
     fig = plt.figure(figsize=(15,15)) 
@@ -505,19 +631,23 @@ def extractBGS(fname, notwilight=True):
     print('%i total BGS fields: ' % len(ssbgs))
     print('approx. BGS coverage [#passes]: %f' % (float(len(ssbgs)) * 7.5 / 14000.)) 
     
-    RAs, DECs = [], [] 
+    tileid, mjd, RAs, DECs = [], [], [], [] 
     airmasses, moon_ills, moon_alts, moon_seps, sun_alts, sun_seps, texps = [], [], [], [], [], [], []
-    seeings, transps = [], [] 
+    snr2frac, ebv, seeings, transps = [], [], [], [] 
     nexps = 0 
     for i in range(len(ssbgs)): 
         isexps  = (ssout['TILEID'] == ssout['TILEID'][iuniq][ssbgs[i]]) 
         nexp    = np.sum(isexps)
         ra      = tiles['RA'][isbgs][bgsss[i]]
         dec     = tiles['DEC'][isbgs][bgsss[i]]
-        mjd     = ssout['MJD'][isexps]
-        # get sky parameters for given ra, dec, and mjd
-        moon_ill, moon_alt, moon_sep, sun_alt, sun_sep = UT.get_thetaSky(np.repeat(ra, nexp), np.repeat(dec, nexp), mjd)
+        _ebv    = tiles['EBV_MED'][isbgs][bgsss[i]]
+        _mjd    = ssout['MJD'][isexps]
 
+        # get sky parameters for given ra, dec, and mjd
+        moon_ill, moon_alt, moon_sep, sun_alt, sun_sep = UT.get_thetaSky(np.repeat(ra, nexp), np.repeat(dec, nexp), _mjd)
+         
+        tileid.append(ssout['TILEID'][isexps]) 
+        mjd.append(_mjd)
         texps.append(ssout['EXPTIME'][isexps]) 
         RAs.append(np.repeat(ra, nexp)) 
         DECs.append(np.repeat(dec, nexp)) 
@@ -527,6 +657,8 @@ def extractBGS(fname, notwilight=True):
         moon_seps.append(moon_sep)
         sun_alts.append(sun_alt) 
         sun_seps.append(sun_sep) 
+        snr2frac.append(ssout['SNR2FRAC'][isexps])
+        ebv.append(np.repeat(_ebv, nexp)) 
         
         # atmospheric seeing and transp
         seeings.append(ssout['SEEING'][isexps]) 
@@ -535,6 +667,8 @@ def extractBGS(fname, notwilight=True):
         nexps += nexp 
 
     exps = {
+        'tileid':   np.concatenate(tileid), 
+        'mjd':      np.concatenate(mjd),
         'texp':     np.concatenate(texps), 
         'ra':       np.concatenate(RAs),
         'dec':      np.concatenate(DECs),
@@ -545,7 +679,9 @@ def extractBGS(fname, notwilight=True):
         'sun_alt':  np.concatenate(sun_alts), 
         'sun_sep':  np.concatenate(sun_seps),
         'seeing':   np.concatenate(seeings),
-        'transp':   np.concatenate(transps)
+        'transp':   np.concatenate(transps),
+        'snr2frac': np.concatenate(snr2frac), 
+        'ebv':      np.concatenate(ebv)
     }
     return exps 
 
@@ -622,6 +758,7 @@ if __name__=="__main__":
             specfile='GALeg.g15.sourceSpec.3000.hdf5', 
             expfile='exposures_surveysim_fork_150sv0p4.fits', 
             seed=0)
+    #surveysim_BGS_texp('exposures_surveysim_fork_150sv0p4.fits')
     #surveysim_BGS('exposures_surveysim_fork_150sv0p4.fits') 
     #surveysim_All('exposures_surveysim_fork_150sv0p4.fits') 
     #surveysim_Weird('exposures_surveysim_fork_150sv0p4.fits') 
