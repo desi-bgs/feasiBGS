5b49e5459d51f8dd25c48b21471aafbe4b216674
chang.nersc <changh20@gmail.com>
2019-12-13 14:54:10 -0800

    constructing meta data table for all the exposures

--- run/survey_validation/cmx_scrape.py
+++ run/survey_validation/cmx_scrape.py
@@ -9,6 +9,7 @@ https://github.com/belaa/cmx_sky/blob/master/sky_level_cmx.ipynb
 '''
 import os
 import glob
+import pickle
 import fitsio
 import numpy as np
 from pathlib import Path
@@ -20,7 +21,6 @@ from desietcimg.db import DB, Exposures
 from desietcimg.util import load_raw
 # -- astro -- 
 import astropy.units as u
-from astroplan import download_IERS_A
 from astropy.time import Time
 from astropy.coordinates import EarthLocation, SkyCoord, AltAz, get_sun, get_moon
 # -- plotting -- 
@@ -58,29 +58,37 @@ if not os.path.exists('db.yaml'):
 def get_skyspectra(night, exp):  
     ''' compile spectra and observing conditions given night and exposure number
     '''
-    spectra = get_spectra(night, exp) 
-
     db = DB() 
     ExpInfo = Exposures(db)
-    texp = ExpInfo(exp, 'mjd_obs')
-    assert texp is not None, "not much we can do if we don't know hwen it's observed..."  
 
-    # find GFA exposure observed closest to the spectra exposure 
-    allexps = db.query("select id,sequence,mjd_obs from exposure").dropna()
-    isGFA = (allexps['sequence'].to_numpy() == 'GFA')
-    GFAmjd = allexps['mjd_obs'].to_numpy()[isGFA]
-    gfa_exp = allexps['id'].to_numpy()[isGFA][np.argmin(np.abs(GFAmjd - ExpInfo(exp, 'mjd_obs')))]
+    # read in metadata of exposure
+    metatable = pickle.load(open('desicmx.exp.metadata.p', 'wb'))
+    assert exp in metatable.keys() 
+    meta = metatable[exp]
 
-    assert np.abs(texp - ExpInfo(gfa_exp, 'mjd_obs')) < 0.001
+    # read in all the spectra
+    spectra = get_spectra(night, exp) 
+    # we assume that most are sky spectra but we remove any outliers 
+    for ispec in range(spectra['flux_b'].shape[0]): 
+        keep = np.ones(spectra['flux_b'].shape[1]).astype(bool)  
+        for band in ['b', 'r', 'z']: 
+            fluxes = spectra['flux_%s' % band][ispec,:,:] 
 
-    meta = {} 
-    for col in ['airmass', 'skyra', 'skydec', 'moonra', 'moondec']: 
-        if ExpInfo(exp, col) is not None: 
-            meta[col] = ExpInfo(exp, col)
+            med_flux = np.median(fluxes, axis=1)
+            std_flux = np.std(med_flux)
+
+            clipped = (med_flux < np.median(med_flux) - 3 * std_flux) | (med_flux > np.median(med_flux) + 3 * std_flux) 
+            keep = keep & ~clipped
+    
+        if ispec == 0: 
+            keeps = [keep] 
+        elif ispec == 3: 
+            # remove spectrograph 3, which has the highest chance of observing stars 
+            keeps.append(np.zeros(spectra['flux_b'].shape[1]).astype(bool))
         else: 
-            meta[col] = ExpInfo(gfa_exp, col)
-    print(meta)      
-    return None
+            keeps.append(keep) 
+    spectra['keep'] = np.array(keeps)
+    return spectra, meta 
 
 
 def get_spectra(night, exp):
@@ -116,24 +124,27 @@ def get_spectra(night, exp):
 def get_obscond(ra, dec, mjd):  
     ''' given RA, Dec, and time of observation compile all observing conditions 
     '''
-    download_IERS_A()
+    #from astroplan import download_IERS_A
+    #download_IERS_A()
+
     # target coordinates 
     coord = SkyCoord(ra=ra * u.deg, dec=dec * u.deg) 
     # observed time (UTC)          
-    utc_time = Time(mjd)
+    utc_time = Time(mjd, format='mjd')
     kpno = EarthLocation.of_site('kitt peak')
 
     kpno_altaz = AltAz(obstime=utc_time, location=kpno) 
     coord_altaz = coord.transform_to(kpno_altaz)
     
     obscond = {} 
-    obscond['airmass'] = coord_altaz.secz
+    obscond['airmass'] = coord_altaz.secz.value
     obscond['elc_lat'] = coord.barycentrictrueecliptic.lat.deg
     obscond['gal_lat'] = coord.galactic.l.deg   # galactic latitude ( used for ISL contribution ) 
     obscond['gal_lon'] = coord.galactic.b.deg   # galactic longitude ( used for ISL contribution ) 
 
     tai = utc_time.tai   
 
+    # twilight contribution
     sun = get_sun(utc_time) 
     sun_altaz   = sun.transform_to(kpno_altaz) 
     obscond['sunalt'] = sun_altaz.alt.deg # sun altitude (degrees)
@@ -148,8 +159,62 @@ def get_obscond(ra, dec, mjd):
     elongation  = sun.separation(moon)
     phase       = np.arctan2(sun.distance * np.sin(elongation), moon.distance - sun.distance*np.cos(elongation))
     obscond['moon_phase']   = phase.value
-    obscond['moon_ill']     = (1. + np.cos(phase))/2.
+    obscond['moon_ill']     = (1. + np.cos(phase.value))/2.
+
     return obscond
+
+
+def make_obscond_table():  
+    ''' scape additional observing conditions for all the exposures so far
+    '''
+    db = DB()
+    ExpInfo = Exposures(db)
+    # compile all the exposures with GFA or specotrograph exposures 
+    allexps = db.query("select id,night,sequence,mjd_obs from exposure").dropna()
+    is_gfa          = (allexps['sequence'].to_numpy() == 'GFA')
+    is_spec         = (allexps['sequence'].to_numpy() == 'Spectrographs')
+    is_gfa_or_spec  = is_gfa | is_spec
+    print('%i nights with GFA or spectra' % len(np.unique(allexps['night'].to_numpy().astype(int)[is_gfa_or_spec])))
+    print('%i GFA or spectra exposures' % np.sum(is_gfa_or_spec))
+    print('%i GFA exposures' % np.sum(is_gfa))
+    print('%i spectra exposures' % np.sum(is_spec))
+
+    mjd_gfa = allexps['mjd_obs'].to_numpy()[is_gfa]
+
+    exps = allexps['id'].to_numpy().astype(int)[is_gfa_or_spec]
+    
+    metadata = {}
+    for exp in exps: 
+        gfa_exp = None 
+        if ExpInfo(exp, 'sequence') == 'spectrographs': 
+            # find nearest GFA exposure 
+            _gfa_exp = allexps['id'].to_numpy().astype(int)[is_gfa][np.argmin(np.abs(mjd_gfa - ExpInfo(exp, 'mjd_obs')))]
+            if np.abs(texp - ExpInfo(_gfa_exp, 'mjd_obs')) < 0.001:  
+                gfa_exp = _gfa_exp
+
+        texp = ExpInfo(exp, 'mjd_obs')
+        meta = {} 
+        meta['mjd'] = texp 
+        for col in ['airmass', 'skyra', 'skydec', 'moonra', 'moondec']: 
+            if ExpInfo(exp, col) is not None: 
+                meta[col] = ExpInfo(exp, col)
+            elif (ExpInfo(exp, 'sequence') == 'Spectrographs') and gfa_exp is not None: 
+                meta[col] = ExpInfo(gfa_exp, col)
+            else: 
+                meta[col] = None 
+
+        if meta['skyra'] is not None and meta['skydec'] is not None and meta['mjd'] is not None: 
+            # append additional observing conditions  
+            #obscond = get_obscond(meta['skyra'], meta['skydec'], meta['mjd']) 
+            #for k in obscond.keys(): 
+            #    meta[k] = obscond[k]
+            meta['night']       = ExpInfo(exp, 'night')
+            meta['mjd']         = ExpInfo(exp, 'mjd_obs')
+            meta['sequence']    = ExpInfo(exp, 'sequence')
+            metadata[exp] = meta 
+    
+    pickle.dump(metadata, open('desicmx.exp.metadata.p', 'wb'))
+    return None
 
 
 if __name__=='__main__': 
@@ -164,4 +229,5 @@ if __name__=='__main__':
     night = 20191112
     #get_spectra(night, 27339)
     #gfa_calib = process_darks(night, 27337, 27338)
-    get_skyspectra(night, 27339)
+    #get_skyspectra(night, 27339)
+    make_obscond_table()
