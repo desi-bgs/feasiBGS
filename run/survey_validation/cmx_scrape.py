9c9e6a8422c9c13fccda1856c3a681db81607c41
chang.nersc <changh20@gmail.com>
2019-12-13 18:47:25 -0800

    compiled table of observing conditions implemented; dark sky script implemented

--- run/survey_validation/cmx_scrape.py
+++ run/survey_validation/cmx_scrape.py
@@ -12,7 +12,6 @@ import glob
 import pickle
 import fitsio
 import numpy as np
-from pathlib import Path
 # -- desi --
 import speclite
 import specsim.atmosphere
@@ -41,7 +40,8 @@ mpl.rcParams['legend.frameon'] = False
 
 
 assert os.environ['NERSC_HOST'] == 'cori'
-dir_specdata = '/project/projectdirs/desi/spectro/data/'
+
+kpno = EarthLocation.of_site('kitt peak')
 
 if not os.path.exists('db.yaml'):
     import getpass
@@ -55,49 +55,153 @@ if not os.path.exists('db.yaml'):
     print('Created db.yaml')
 
 
-def get_skyspectra(night, exp):  
-    ''' compile spectra and observing conditions given night and exposure number
+def darksky():
+    ''' compile dark sky spectra and compare with dark sky model to approximate
+    flux calibrations for the spectrographs 
     '''
     db = DB() 
     ExpInfo = Exposures(db)
 
     # read in metadata of exposure
-    metatable = pickle.load(open('desicmx.exp.metadata.p', 'wb'))
-    assert exp in metatable.keys() 
-    meta = metatable[exp]
+    meta = pickle.load(open('desicmx.exp.metadata.p', 'rb'))
 
+    for ispec in range(10): 
+        _i = 0 
+        waves, fluxes = {}, {} 
+    
+        # dark sky  
+        isdark = (
+                (meta['sequence'] == 'Spectrographs') &  
+                (meta['moon_alt'] < 0.) & 
+                (meta['airmass'] < 1.1)
+                )
+        nights  = meta['night'][isdark]
+        exps    = meta['exp'][isdark]
+
+        for night, exp in zip(nights, exps): 
+            try: 
+                spectra = get_spectra(meta['night'], exp, type='sky')
+            except AssertionError: 
+                continue 
+
+            issky = spectra['keep'][ispec]
+            if np.sum(issky) == 0: continue  # no sky spectra in this petal 
+            print('%i spectra from %i (exp %i)' % (np.sum(issky), meta['night'], exp)) 
+
+            for band in ['b', 'r', 'z']: 
+                if _i == 0: 
+                    waves[band] = spectra['wave_%s' % band][ispec,issky,:] 
+                    fluxes[band] = spectra['flux_%s' % band][ispec,issky,:]
+                else: 
+                    waves[band] = np.concatenate([waves[band], spectra['wave_%s' % band][ispec,issky,:]]) 
+                    fluxes[band] = np.concatenate([fluxes[band], spectra['flux_%s' % band][ispec,issky,:]]) 
+            _i += 1 
+        if _i == 0: continue 
+        
+        fig = plt.figure(figsize=(10,5))
+        sub = fig.add_subplot(111)
+        for ib, band in enumerate(['b', 'r', 'z']): 
+            for i in np.arange(waves[band].shape[0])[::10]: 
+                sub.plot(waves[band][i,:], fluxes[band][i,:], c='C%i' % ib, alpha=0.1)
+        sub.set_xlim(3500, 9800) 
+        sub.set_ylim(0, 500) 
+        fig.savefig('desicmx.darksky.spectrograph%i.png' % ispec, bbox_inches='tight') 
+    return None 
+
+
+def maybe():
+    ''' compile dark sky spectra and compare with dark sky model to approximate
+    flux calibrations for the spectrographs 
+    '''
+    db = DB() 
+    ExpInfo = Exposures(db)
+
+    # read in metadata of exposure
+    meta = pickle.load(open('desicmx.exp.metadata.p', 'rb'))
+
+    for ispec in range(10): 
+        _i = 0 
+        waves, fluxes = {}, {} 
+    
+        # dark sky  
+        isdark = (
+                (meta['sequence'] == 'Spectrographs') &  
+                (meta['moon_alt'] < 0.) & 
+                (meta['airmass'] < 1.1)
+                )
+        nights  = meta['night'][isdark]
+        exps    = meta['exp'][isdark]
+
+        for night, exp in zip(nights, exps): 
+            try: 
+                spectra = get_spectra(meta['night'], exp, type='maybe')
+            except AssertionError: 
+                continue 
+
+            issky = spectra['keep'][ispec]
+            if np.sum(issky) == 0: continue  # no sky spectra in this petal 
+            print('%i spectra from %i (exp %i)' % (np.sum(issky), meta['night'], exp)) 
+
+            for band in ['b', 'r', 'z']: 
+                if _i == 0: 
+                    waves[band] = spectra['wave_%s' % band][ispec,issky,:] 
+                    fluxes[band] = spectra['flux_%s' % band][ispec,issky,:]
+                else: 
+                    waves[band] = np.concatenate([waves[band], spectra['wave_%s' % band][ispec,issky,:]]) 
+                    fluxes[band] = np.concatenate([fluxes[band], spectra['flux_%s' % band][ispec,issky,:]]) 
+            _i += 1 
+        if _i == 0: continue 
+        
+        fig = plt.figure(figsize=(10,5))
+        sub = fig.add_subplot(111)
+        for ib, band in enumerate(['b', 'r', 'z']): 
+            for i in np.arange(waves[band].shape[0])[::10]: 
+                sub.plot(waves[band][i,:], fluxes[band][i,:], c='C%i' % ib, alpha=0.1)
+        sub.set_xlim(3500, 9800) 
+        sub.set_ylim(0, 500) 
+        fig.savefig('desicmx.maybe.spectrograph%i.png' % ispec, bbox_inches='tight') 
+    return None 
+
+
+def get_spectra(night, exp, type='sky'):  
+    ''' compile spectra and observing conditions given night and exposure number
+    '''
     # read in all the spectra
-    spectra = get_spectra(night, exp) 
+    spectra = _get_spectra(night, exp) 
+
     # we assume that most are sky spectra but we remove any outliers 
-    for ispec in range(spectra['flux_b'].shape[0]): 
+    keeps = np.zeros((spectra['flux_b'].shape[0], spectra['flux_b'].shape[1])).astype(bool) 
+    for ispec in range(10): 
+        if (ispec == 3): continue  # remove spectrograph 3, which has the highest chance of observing stars 
+        if (spectra['flux_b'][ispec,0,0] == -999.): continue 
+
         keep = np.ones(spectra['flux_b'].shape[1]).astype(bool)  
         for band in ['b', 'r', 'z']: 
             fluxes = spectra['flux_%s' % band][ispec,:,:] 
 
             med_flux = np.median(fluxes, axis=1)
             std_flux = np.std(med_flux)
-
-            clipped = (med_flux < np.median(med_flux) - 3 * std_flux) | (med_flux > np.median(med_flux) + 3 * std_flux) 
+            
+            if type == 'sky': 
+                clipped = (med_flux < np.median(med_flux) - std_flux) | (med_flux > np.median(med_flux) + std_flux) 
+            elif type == 'maybe': 
+                clipped = (med_flux < np.median(med_flux) + 2 * std_flux) 
             keep = keep & ~clipped
     
-        if ispec == 0: 
-            keeps = [keep] 
-        elif ispec == 3: 
-            # remove spectrograph 3, which has the highest chance of observing stars 
-            keeps.append(np.zeros(spectra['flux_b'].shape[1]).astype(bool))
-        else: 
-            keeps.append(keep) 
-    spectra['keep'] = np.array(keeps)
-    return spectra, meta 
+        keeps[ispec,:] = keep
+
+    spectra['keep'] = keeps
+    return spectra
 
 
-def get_spectra(night, exp):
+def _get_spectra(night, exp):
     ''' given the night and exposure number get wavelength, flux, and spectrograph 
     '''
     dir_spec = '/project/projectdirs/desi/spectro/nightwatch/kpno/' 
     
     spectra = {} 
     for band in ['b', 'r', 'z']: 
+        _i = 0 
         for ispec in range(10): # spectrograph number 
             fspec = os.path.join(dir_spec, '%i' % night, '000%i' % exp, 'qframe-%s%i-000%i.fits' % (band, ispec, exp))
             if not os.path.exists(fspec): continue 
@@ -105,25 +209,25 @@ def get_spectra(night, exp):
             flux = fitsio.read(fspec, 'FLUX') 
             wave = fitsio.read(fspec, 'WAVELENGTH') 
 
-            if ispec == 0: 
-                fluxes  = [flux] 
-                waves   = [wave] 
-                ispecs  = [ispec]
-            else: 
-                fluxes.append(flux) 
-                waves.append(wave) 
-                ispecs.append(ispec) 
+            if _i == 0: 
+                fluxes  = np.tile(-999., (10, flux.shape[0], flux.shape[1]))
+                waves   = np.tile(-999., (10, wave.shape[0], wave.shape[1]))
+            fluxes[ispec,:,:] = flux
+            waves[ispec,:,:] = wave
+            _i += 1 
 
-        spectra['flux_%s' % band] = np.array(fluxes) 
-        spectra['wave_%s' % band] = np.array(waves) 
-        spectra['spectrograph'] = np.array(ispecs) 
-
+        assert _i > 0, 'no spectra files'
+        spectra['flux_%s' % band] = fluxes
+        spectra['wave_%s' % band] = waves 
     return spectra 
 
 
 def get_obscond(ra, dec, mjd):  
     ''' given RA, Dec, and time of observation compile all observing conditions 
     '''
+    #from astropy.utils.iers import IERS_A_URL_MIRROR
+    #IERS_A_URL_MIRROR() 
+
     #from astroplan import download_IERS_A
     #download_IERS_A()
 
@@ -131,9 +235,9 @@ def get_obscond(ra, dec, mjd):
     coord = SkyCoord(ra=ra * u.deg, dec=dec * u.deg) 
     # observed time (UTC)          
     utc_time = Time(mjd, format='mjd')
-    kpno = EarthLocation.of_site('kitt peak')
 
     kpno_altaz = AltAz(obstime=utc_time, location=kpno) 
+
     coord_altaz = coord.transform_to(kpno_altaz)
     
     obscond = {} 
@@ -160,7 +264,6 @@ def get_obscond(ra, dec, mjd):
     phase       = np.arctan2(sun.distance * np.sin(elongation), moon.distance - sun.distance*np.cos(elongation))
     obscond['moon_phase']   = phase.value
     obscond['moon_ill']     = (1. + np.cos(phase.value))/2.
-
     return obscond
 
 
@@ -182,7 +285,8 @@ def make_obscond_table():
     mjd_gfa = allexps['mjd_obs'].to_numpy()[is_gfa]
 
     exps = allexps['id'].to_numpy().astype(int)[is_gfa_or_spec]
-    
+
+    _i = 0     
     metadata = {}
     for exp in exps: 
         gfa_exp = None 
@@ -204,30 +308,27 @@ def make_obscond_table():
                 meta[col] = None 
 
         if meta['skyra'] is not None and meta['skydec'] is not None and meta['mjd'] is not None: 
+            print(exp) 
             # append additional observing conditions  
-            #obscond = get_obscond(meta['skyra'], meta['skydec'], meta['mjd']) 
-            #for k in obscond.keys(): 
-            #    meta[k] = obscond[k]
+            obscond = get_obscond(meta['skyra'], meta['skydec'], meta['mjd']) 
+            for k in obscond.keys(): 
+                meta[k] = obscond[k]
+            meta['exp']         = exp
             meta['night']       = ExpInfo(exp, 'night')
             meta['mjd']         = ExpInfo(exp, 'mjd_obs')
             meta['sequence']    = ExpInfo(exp, 'sequence')
-            metadata[exp] = meta 
+            if _i == 0: 
+                for k in meta.keys(): 
+                    metadata[k] = [meta[k]] 
+            for k in meta.keys(): 
+                metadata[k].append(meta[k]) 
+            _i += 1 
     
+    for k in metadata.keys(): 
+        metadata[k] = np.array(metadata[k])
     pickle.dump(metadata, open('desicmx.exp.metadata.p', 'wb'))
     return None
 
 
 if __name__=='__main__': 
-    gfa_exp = [27337, 27338, 27340, 27341, 27342, 27343, 27344, 27346, 27347, 27348, 27349, 27350,
-           27352, 27353, 273524, 27355, 27356, 27358, 27359, 27360, 27361, 27362, 27364, 27365,
-           27366, 27367, 27368, 27369, 27371, 27372, 27373, 27374, 27376, 27377, 27378, 27379,
-           27380, 27382, 27383, 27384, 27385, 27386, 27388, 27389, 27390, 27391, 27392, 27394,
-           27395, 27396]
-
-    spec_exp = [27339, 27345, 27351, 27357, 27363, 27370, 27375, 27381, 27387, 27393]
-
-    night = 20191112
-    #get_spectra(night, 27339)
-    #gfa_calib = process_darks(night, 27337, 27338)
-    #get_skyspectra(night, 27339)
     make_obscond_table()
