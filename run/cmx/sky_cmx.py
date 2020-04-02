5905a43d396f4dbe4f3269001ed8d216961cd26f
changhoonhahn <changhoonhahn@lbl.gov>
2020-04-02 08:38:40 -0700

    examining the transparencies of exposures 70502 and 66000

--- run/cmx/sky_cmx.py
+++ run/cmx/sky_cmx.py
@@ -52,7 +52,8 @@ def compile_skies():
     sky_data = {} 
     for k in ['airmass', 'moon_ill', 'moon_alt', 'moon_sep', 'sun_alt',
             'sun_sep', 'flux_b', 'flux_r', 'flux_z', 'sky_b', 'sky_r', 'sky_z',
-            'tileid', 'date', 'expid', 'spectrograph', 'mjd', 'transparency', 'exptime']:  
+            'tileid', 'date', 'expid', 'spectrograph', 'mjd', 'transparency',
+            'transp_min', 'exptime']:  
         sky_data[k] = [] 
 
     for i, _cmx in enumerate(cmx): 
@@ -110,8 +111,10 @@ def compile_skies():
         transp = gfa['TRANSPARENCY'][m_gfa]
         not_nan = np.isfinite(transp)
         if np.sum(not_nan) > 0: 
+            transp_min = np.min(transp[not_nan])
             transp_mid = np.median(transp[not_nan])
         else: 
+            transp_min = 0.
             transp_mid = 1.
         
         # these values more or less agree with the GFA values 
@@ -122,6 +125,8 @@ def compile_skies():
         print('%.f exptime' % exptime)
         print('%i sky fibers' % np.sum(is_sky)) 
         print('%i good sky fibers' % np.sum(good_sky))
+        print('%.2f < transp < %.2f' % (transp[not_nan].min(),
+            transp[not_nan].max()))
 
         sky_data['tileid'].append(np.repeat(_tileid, len(_airmass)))
         sky_data['date'].append(np.repeat(_date, len(_airmass)))
@@ -129,6 +134,7 @@ def compile_skies():
         sky_data['spectrograph'].append(np.repeat(_spec, len(_airmass)))
         sky_data['mjd'].append(np.repeat(mjd_mid, len(_airmass)))
         sky_data['transparency'].append(np.repeat(transp_mid, len(_airmass)))
+        sky_data['transp_miin'].append(np.repeat(transp_min, len(_airmass)))
         sky_data['exptime'].append(np.repeat(exptime, len(_airmass)))
         # store fluxes 
         sky_data['flux_b'].append(cframe_b[good_sky,:]) 
